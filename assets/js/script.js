ce74738c06d2b52ee1dbb8e48c20baa9aa176a9f
kjsooraj <104343906+kjsooraj@users.noreply.github.com>
2022-06-29 17:09:52 +0530

    header and related func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const doc = document.querySelector("html");
 const toggle = document.querySelector("#theme");
+const img = document.querySelector(".ca-header__logo");
 
 const loadTheme = () => {
   return window.matchMedia &&
@@ -19,6 +20,13 @@ const setTheme = (theme) => {
   if (typeof toggle !== "undefined" && toggle !== null) {
     theme === "dark" ? (toggle.checked = true) : (toggle.checked = false);
   }
+
+  img.setAttribute(
+    "src",
+    theme === "dark"
+      ? "../assets/images/creative-agency-dark.svg"
+      : "../assets/images/creative-agency-light.svg"
+  );
 };
 
 toggle &&
@@ -37,3 +45,29 @@ window
 window.addEventListener("load", () => {
   setTheme(loadTheme());
 });
+
+// header mobile menu toggle
+const hamburger = document.querySelector("[data-hamburger]");
+const header = document.querySelector(".ca-header");
+const nav = document.querySelector(".ca-header__nav");
+
+const mobileMenuTrigger = () => {
+  const nav = document.querySelector(".ca-header__nav");
+  nav.classList.toggle("ca-header__nav--show");
+  const { height } = header.getBoundingClientRect();
+  nav.style.top = height + "px";
+  nav.classList.contains("ca-header__nav--show")
+    ? hamburger.classList.add("ca-header__hamburger--open")
+    : hamburger.classList.remove("ca-header__hamburger--open");
+};
+
+hamburger.addEventListener("click", mobileMenuTrigger);
+
+// change header background on scroll
+const changeHeaderBackground = (e) => {
+  window.scrollY > 0
+    ? header.classList.add("ca-header--scrolled")
+    : header.classList.remove("ca-header--scrolled");
+};
+
+window.addEventListener("scroll", changeHeaderBackground);
